c5cace66e0eaf8f7775b8ecd7e0458b5962d8414
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-11-27 11:30:24 -0600

    finished

--- analyzer.rb
+++ analyzer.rb
@@ -8,6 +8,14 @@ paragraph_count = text.split(/\n\n/).length
 sentence_count = text.split(/\.|\?|!/).length
 # This is shortcut notation for creating an array.
 stop_words = %w{the a by on for of are with just but and to the my I has some in}
+all_words = text.scan(/\w+/)
+good_words = all_words.select { |word| !stop_words.include?(word) }
+good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
+sentences = text.gsub(/\s+/, " ").strip.split(/\.|\?|!/)
+sentences_sorted = sentences.sort_by { |sentence| sentence.length }
+one_third = sentences_sorted.length / 3
+ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
+ideal_sentences = ideal_sentences.select { |sentence| sentence =~ /is|are/ }
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
@@ -17,3 +25,6 @@ puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count} sentences"
 puts "#{sentence_count/paragraph_count} sentences per paragraph (average)"
 puts "#{word_count/sentence_count} words per sentence (average)"
+puts "#{good_percentage}% of words are non-fluff words"
+puts "Summary:\n\n" + ideal_sentences.join(". ")
+puts "-- End of analysis"
